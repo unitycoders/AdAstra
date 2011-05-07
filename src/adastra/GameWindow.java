13fc742e051648ad2c3b7b6863e42ff3b59bf5d8
Joseph Walton-Rivers <webpigeon@gmail.com>
2011-05-07 21:42:38 +0100

    added main window

---
+++ src/adastra/GameWindow.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adastra;
+
+import javax.swing.JFrame;
+
+import java.awt.Dimension;
+
+/**
+ *
+ * @author webpigeon
+ */
+public class GameWindow {
+    private JFrame frame;
+    
+    public GameWindow(){
+        //Build a frame
+        frame = new JFrame("Game Window");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(800,600));
+        
+        //Frame
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
