88cc072e6052fae80bb6838aba5b5c81daa02061
Joseph Walton-Rivers <webpigeon@gmail.com>
2011-05-07 22:59:12 +0100

    added UI Design for Planet Management

---
+++ src/adastra/engine/ShipyardSettings.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adastra.engine;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author webpigeon
+ */
+public class ShipyardSettings extends JPanel{
+    
+}
