649bc1c175005c50acccf83821f144263053e63f
Joseph Walton-Rivers <webpigeon@gmail.com>
2011-05-20 20:43:21 +0100

    added loader interface prototype

---
+++ src/adastra/engine/loader/Loader.java
@@ -0,0 +1,13 @@
+
+package adastra.engine.loader;
+
+public interface Loader{
+
+	public PlanetType[] getPlanetTypes();
+
+	//TODO implement StarType
+	//public StarType[] getStarType();
+
+	//TODO implement TechTree
+	public Technology getTechTree();
+}
